9aed2353979799008515692f10e1f85e5dc31180
Brendan Guillory <brendanguillory@gmail.com>
2021-04-22 15:44:28 -0500

    Added several changes
    Updates include:
    > Pieces are now dropped based on when you press your button, not where your mouse is
    > You can pass a 1 or 2 to the program to select your piece
    > A function is in place for determining the opponent's move. It is stupid right now.

--- connect-4.py
+++ connect-4.py
@@ -10,6 +10,14 @@ COLUMN_COUNT = 7
 PLAYER_1_PIECE = 1
 PLAYER_2_PIECE = 2
 
+# Which piece are we? This will likely be 1 or 2.
+# This is passed in with the runner scipt.
+MY_PIECE = 1
+if len(sys.argv) < 2:
+    print("No arguments passed. Defaulting to piece 1: red.")
+else:
+    MY_PIECE = int(sys.argv[1])
+
 # Size of each square on the screen (in pixels)
 SQUARE_SIZE = 100
 CIRCLE_RADIUS = (SQUARE_SIZE // 2) - 5
@@ -19,6 +27,17 @@ BLUE = (0,0,255)
 BLACK = (0,0,0)
 RED = (255,0,0)
 YELLOW = (255,255,0)
+
+# Some derivative variables
+# 3 - MY_PIECE only works if the pieces are 1 and 2.
+OPP_PIECE = 3 - MY_PIECE
+# Our color.
+if MY_PIECE == 1:
+    MY_COLOR = RED
+    OPP_COLOR = YELLOW
+else:
+    MY_COLOR = YELLOW
+    OPP_COLOR = RED
 
 # Function which creates an empty "board" - a 6x7 numpy matrix.
 def create_board():
@@ -117,9 +136,149 @@ def draw_board(board):
     # This needs to be called to actually change what is shown on the screen.
     pygame.display.update()
 
-board = create_board()
-game_over = False
-turn = 1
+# Check for important events (like the mouse button being clicked, the GPIO
+# button being pressed, or the QUIT signal). This is run after the chip changes
+# position over the top of the board.
+def important_event_happened():
+    for event in pygame.event.get():
+
+        # If the user has quit the game, terminate immediately.
+        if event.type == pygame.QUIT:
+            sys.exit()
+
+        # If the user clicked the mouse or button, return true.
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            return True
+
+    # We didn't find anything worth writing home about. Return False.
+    return False
+
+# This import is temporary! Remove when proper networking is added!
+import random
+def get_opponent_move():
+    # This is where we will eventually put some fancy networking.
+    # For now, just return 0.
+    return random.randint(0, 6)
+
+def play_game():
+
+    # Game variables!
+    # The board. A numpy matrix.
+    board = create_board()
+    # The game's state. The game is not over by default.
+    game_over = False
+    # Which turn is it? This increments each time someone places a piece down.
+    turn = 1
+    # which spot over the board is the piece hovering? Changes many times
+    # per turn.
+    piece_col = 0
+    # Is the piece moving right or left? 1 for right, -1 for left.
+    piece_direction = 1
+
+
+    draw_board(board)
+    pygame.display.update()
+    
+    while not game_over:
+
+        # Draw the board as it currently is.
+        draw_board(board)
+
+        if 2 - (turn % 2) == MY_PIECE:
+            # It's player 1's turn! Start moving the piece over the top of the
+            # board.
+            
+            # How long should we wait before changing the position of the piece? (ms)
+            # This time starts off at 500ms, then goes down by 25ms for each
+            # turn until it reaches 100ms. It then stays there fore the rest
+            # of the game.
+            if turn <= 19:
+                position_change_delay = 500 - (25 * (turn - 1))
+            else:
+                position_change_delay = 100
+            
+            is_our_turn = True
+            while is_our_turn:
+
+                # Remove the previous piece's circle on the screen.
+                draw_top_row()
+
+                # Determine the piece's position and draw it.
+                circle_pos = ((piece_col * SQUARE_SIZE) + (SQUARE_SIZE // 2), SQUARE_SIZE // 2)
+                pygame.draw.circle(screen, MY_COLOR, circle_pos, CIRCLE_RADIUS)
+                pygame.display.update()
+
+                # Wait some time.
+                pygame.time.wait(position_change_delay)
+                # Check for important events
+                if important_event_happened():
+
+                    # Looks like the user clicked the mouse or the big red
+                    # button! See if we can execute that move.
+                    if is_valid_location(board, piece_col):
+                        # It is! Place the piece.
+                        row = get_next_open_row(board, piece_col)
+                        drop_piece(board, row, piece_col, MY_PIECE)
+                        # Draw the top row to get rid of the piece that was
+                        # just there.
+                        draw_top_row()
+                        draw_board(board)
+                        pygame.display.update()
+                        is_our_turn = False
+                        turn += 1
+
+                        # Check to see if the game is over.
+                        if winning_move(board, MY_PIECE):
+                            # It is!
+                            # Make the label which will be displayed.
+                            label = text_font.render("Player {} wins!".format(MY_PIECE), 1, MY_COLOR)
+                            # display the label.
+                            screen.blit(label, (40, 10))
+                            pygame.display.update()
+                            game_over = True
+
+                # If we get here, nothing interesting happened since we started
+                # wait()ing. Move the piece to the next column.
+                else:
+                    if piece_col == 6 and piece_direction == 1:
+                        # We reached the right side of the board.
+                        # Move to col 5 and change direction.
+                        piece_direction = -1
+                        piece_col = 5
+                    elif piece_col == 0 and piece_direction == -1:
+                        # We reached the left side of the board.
+                        # Move to col 1 and change direction.
+                        piece_direction = 1
+                        piece_col = 1
+                    else:
+                        piece_col += piece_direction
+
+        else:
+            # Looks like it's not our turn. Get (read: wait for) the opponent's
+            # move, then do the normal operations on it. Assuming the other
+            # player isn't cheating in some way, this will be a guaranteed
+            # good move.
+            opp_move = get_opponent_move()
+            row = get_next_open_row(board, opp_move)
+            drop_piece(board, row, opp_move, OPP_PIECE)
+            pygame.display.update()
+
+            # Check if the other player has just won.
+            if winning_move(board, OPP_PIECE):
+                # They did! gg!
+                label = text_font.render("Player {} wins!".format(OPP_PIECE), 1, OPP_COLOR)
+                # Display the label.
+                screen.blit(label, (40, 10))
+                pygame.display.update()
+                game_over = True
+
+            # Increment the turn counter.
+            turn += 1
+
+    # Looks like the game just ended!
+    # Let's wait a few seconds to let this sink in.
+    pygame.time.wait(5000)
+    
 
 # initialize pygame
 pygame.init()
@@ -130,65 +289,15 @@ screen_height = (ROW_COUNT + 1) * SQUARE_SIZE
 screen_size = (screen_width, screen_height)
 
 screen = pygame.display.set_mode(screen_size)
-draw_board(board)
-pygame.display.update()
 
 text_font = pygame.font.SysFont("monospace", 75)
 
-while not game_over:
+while True:
 
-    for event in pygame.event.get():
+    # Clear any events from the queue. This prevents people from placing chips
+    # in-between games.
+    pygame.event.clear()
 
-        # Two boilerplate lines. If pygame says to quit, quit.
-        if event.type == pygame.QUIT:
-            sys.exit()
+    # start the game! Lessss gooo!
+    play_game()
 
-        if event.type == pygame.MOUSEMOTION:
-            # Fill the top row with a big BLACK rectangle to get rid
-            # of the previous chip on the top.
-            draw_top_row()
-            pos_x = event.pos[0]
-            if turn % 2 == 1:
-                pygame.draw.circle(screen, RED, (pos_x, SQUARE_SIZE // 2), CIRCLE_RADIUS)
-            else:
-                pygame.draw.circle(screen, YELLOW, (pos_x, SQUARE_SIZE // 2), CIRCLE_RADIUS)
-            pygame.display.update()
-            continue
-        
-        # Now onto the more interesting stuff. Check for mouse down.
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            pos_x = event.pos[0]
-            
-            # ask player 1 for input
-            if turn % 2 == 1:
-                col = pos_x // SQUARE_SIZE
-                if is_valid_location(board, col):
-                    row = get_next_open_row(board, col)
-                    drop_piece(board, row, col, PLAYER_1_PIECE)
-                    turn += 1
-
-                    if winning_move(board, PLAYER_1_PIECE):
-                        draw_top_row()
-                        label = text_font.render("Player 1 wins!", 1, RED)
-                        screen.blit(label, (40, 10))
-                        game_over = True
-
-            # ask player 2 for input
-            else:
-                col = pos_x // SQUARE_SIZE
-                if is_valid_location(board, col):
-                    row = get_next_open_row(board, col)
-                    drop_piece(board, row, col, PLAYER_2_PIECE)
-                    turn += 1
-
-                    if winning_move(board, PLAYER_2_PIECE):
-                        draw_top_row()
-                        label = text_font.render("Player 2 wins!", 1, YELLOW)
-                        screen.blit(label, (40, 10))
-                        game_over = True
-
-            # print_board(board)
-            draw_board(board)
-
-            if game_over:
-                pygame.time.wait(3000)
